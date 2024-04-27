00f835cee270c56472a27233281e1564cd7d175d
Eduardo Rios Sale <edu.90@live.com.ar>
2024-04-27 01:29:48 -0300

    Nuevos cambios

--- js/main.js
+++ js/main.js
@@ -30,9 +30,9 @@ btnColorMode.addEventListener("click", () => {
     document.body.classList.toggle("dark-mode");
 
     if (document.body.classList.contains("dark-mode")) {
-        btnColorMode.innerText = "☀️";
-    } else {
         btnColorMode.innerText = "🌙";
+    } else {
+        btnColorMode.innerText = "☀️";
     }
 })
 
